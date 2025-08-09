15e2456fa678435ffd79655f0ea3dfc3eb962b52
David Ross <dross365@Mac.lan>
2025-08-09 10:18:39 -0400

    Added More To Contact Section

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -7,8 +7,17 @@ const ContactSection = () => {
       <h2 className="uppercase tracking-widest text-2xl text-center mt-2">
         Contact Us
       </h2>
-      <p className="text-sm md:text-lg w-[90%] md:w-[80%] mx-auto text-center mt-4">
+      <p className="text-md font-semibold uppercase md:text-lg w-[90%] md:w-[80%] mx-auto text-center mt-4">
+        general
+      </p>
+      <p className="text-sm md:text-lg w-[90%] md:w-[80%] mx-auto text-center mb-2">
         info@misspollystearoom.com
+      </p>
+      <p className="text-md font-semibold uppercase md:text-lg w-[90%] md:w-[80%] mx-auto text-center mt-4">
+        events
+      </p>
+      <p className="text-sm md:text-lg w-[90%] md:w-[80%] mx-auto text-center mb-2">
+        events@misspollystearoom.com
       </p>
     </section>
   );
