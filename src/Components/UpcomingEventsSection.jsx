80effac63672ac344d94100682a1268cf03533bf
dross365 <dross98.fullstackdev@gmail.com>
2026-02-20 11:15:11 -0500

    deleted feb event

--- src/Components/UpcomingEventsSection.jsx
+++ src/Components/UpcomingEventsSection.jsx
@@ -10,10 +10,10 @@ const UpcomingEventsSection = () => {
           upcoming events
         </h2>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 pb-4 pt-4'>
+      <div className='grid grid-cols-1 pb-4 pt-4'>
         {' '}
         {/* md:grid-cols-2 with 2 menus */}
-        <div>
+        {/* <div>
           <h2 className='uppercase tracking-widest text-2xl text-center'>
             february event
           </h2>
@@ -22,7 +22,7 @@ const UpcomingEventsSection = () => {
             alt='celeste dining room'
             className='w-72 lg:w-120 h-auto mx-auto rounded-lg shadow-lg my-3'
           />
-        </div>
+        </div> */}
         <div>
           <h2 className='uppercase tracking-widest text-2xl text-center'>
             march event
