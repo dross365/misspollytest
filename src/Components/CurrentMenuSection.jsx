7e57733d6043278a4f608c0c28c939847ce1cd6a
dross365 <dross98.fullstackdev@gmail.com>
2026-05-09 14:40:25 -0400

    may menu

--- src/Components/CurrentMenuSection.jsx
+++ src/Components/CurrentMenuSection.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import currentMenu from '../assets/march_menu.jpeg'
-import aprilMenu from '../assets/april_menu.jpeg'
+import currentMenu from '../assets/mayMenu.jpeg'
 
 const CurrentMenuSection = () => {
   return (
     <section id='currentmenu' className='bg-[#faf3e0] pt-[20px]'>
-      <div className='grid grid-cols-1 pb-4 pt-4 md:grid-cols-2'>
+      <div className='grid grid-cols-1 pb-4 pt-4'>
         {' '}
-        {/* md:grid-cols-2 with 2 menus */}
+        {/* md:grid-cols-2 */} {/* md:grid-cols-2 with 2 menus */}
         <div>
           <h2 className='uppercase tracking-widest lg:text-2xl text-xl text-center'>
             current menu
@@ -18,7 +17,7 @@ const CurrentMenuSection = () => {
             className='w-72 lg:w-120 h-auto mx-auto rounded-lg shadow-lg mt-3 mb-[100px]'
           />
         </div>
-        <div>
+        {/* <div>
           <h2 className='uppercase tracking-widest lg:text-2xl text-xl text-center'>
             april menu
           </h2>
@@ -27,7 +26,7 @@ const CurrentMenuSection = () => {
             alt='april menu'
             className='w-72 lg:w-120 h-auto mx-auto rounded-lg shadow-lg my-3'
           />
-        </div>
+        </div> */}
       </div>
     </section>
   )
